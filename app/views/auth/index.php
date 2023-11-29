baabd8dbfdc0cac978ceae9b3e89e678c7781540
mauldoto <maulanaly97@gmail.com>
2023-11-29 13:31:19 +0700

    change baseurl

--- app/views/auth/index.php
+++ app/views/auth/index.php
@@ -10,19 +10,19 @@
 
         <section class="title-section">
             <div class="col-xs-12 col-md-6 col-md-offset-3 text-center">
-                <img src="<?= BASEURL; ?>/assets/images/logo/logo USTP.png" width="20%" alt="">
+                <img src="<?= ASSETS_URL; ?>/assets/images/logo/logo USTP.png" width="20%" alt="">
             </div>
             <div class="flexx">
                 <div class="col-xs-12 col-md-6 oer">
                     <div class="text-center div-oer">
-                        <img class="img-oer" src="<?= BASEURL; ?>/assets/images/logo/oer-cropped.png" alt="">
+                        <img class="img-oer" src="<?= ASSETS_URL; ?>/assets/images/logo/oer-cropped.png" alt="">
                     </div>
                 </div>
                 <div class="col-xs-12 col-md-6 mb-3">
                     <div class="text-center">
                         <h4><label for="">Login</label></h4>
                     </div>
-                    <form action="<?= BASEURL; ?>/AuthUser/login" method="post">
+                    <form action="<?= BASEURL; ?>/authuser/login" method="post">
                         <div class="form-group">
                             <label for="username">Username</label>
                             <input type="text" class="form-control" id="username" name="username" placeholder="Username">
--- app/views/blockuser/index.php
+++ app/views/blockuser/index.php
@@ -1,7 +1,7 @@
 <?php $post_url = ''; ?>
 
 <div class="logout-btn text-right" style="margin-top: 5px;">
-    <form action="<?= BASEURL; ?>/AuthUser/logout" method="post">
+    <form action="<?= BASEURL; ?>/authuser/logout" method="post">
         <button type="submit" class="btn btn-sm btn-danger">Logout</button>
     </form>
 </div>
@@ -16,13 +16,13 @@
 
         <section class="title-section">
             <div class="col-xs-12 col-sm-12 text-center mb-3">
-                <img src="<?= BASEURL; ?>/assets/images/logo/oer-cropped.png" width="200px" alt="">
+                <img src="<?= ASSETS_URL; ?>/assets/images/logo/oer-cropped.png" width="200px" alt="">
             </div>
             <div class="col-xs-12 col-sm-12 text-center mb-3">
                 <h4>Block User Koperasi</h4>
             </div>
             <div class="col-xs-12 mb-3">
-                <form action="<?= BASEURL; ?>/BlockUser/submit" method="post">
+                <form action="<?= BASEURL; ?>/blockuser/submit" method="post">
                     <div class="form-group">
                         <label for="pegawai">Anggota</label>
                         <select type="text" class="form-control nik-select2" id="pegawai" name="anggota" placeholder="Ketik Nik atau Nama Karyawan disini">
